91c8166c9daa4dc0cb417c6e5a8166e79651d598
Juan Dos Santos Arcari <Juan.Arcari@zaffari.com.br>
2022-06-07 00:12:24 -0300

    iniciando projeto para o curso de Flutter

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    body: Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.0'),
+            subtitle: Text('1800'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('350.0'),
+            subtitle: Text('1320'),
+          ),
+        ),
+      ],
+    ),
+    appBar: AppBar(title: Text('Transferências'),),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Icon(Icons.add),
+    ),
+  ),
+));
